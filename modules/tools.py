30d16822c7fe4feac708fe0e6af8674ec342d8cc
DanteUrdaneta <you@example.com>
2026-05-21 22:20:03 -0400

    changing airtable parameters

--- modules/tools.py
+++ modules/tools.py
@@ -2,8 +2,8 @@ from pyairtable import Api
 
 
 ACCESS_TOKEN = "pat6RbHdPN4t76tu5.ba6cd9280ae7305533c89723eb47e40e4b4790224ce6b854e8c5a7b16a15e208"
-BASE_ID = "appTwuay9eWslDDcs"
-TABLE_NAME = "Inventarios"
+BASE_ID = "app8KjdUhteh6fbEf"
+TABLE_NAME = "RESUMEN"
 
 api = Api(ACCESS_TOKEN)
 table = api.table(BASE_ID, TABLE_NAME)
